423c092bede4d36064d4d9c8a6f3bc9b505ea877
Alex <barbudaniel99@gmail.com>
2021-03-04 12:34:56 +0200

    Seminar 2 changes

--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -1,23 +1,23 @@
 package ro.ase.cts.program;
 
-import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatReader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
 public class Program {
 
-	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
-		try {
-			listaAngajati = Utils.readAngajati("Seminar2/angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        List<Aplicant> listaAplicanti;
+        try {
+            listaAplicanti = AngajatReader.readAngajati("Seminar2/angajati.txt");
+            for (Aplicant aplicant : listaAplicanti)
+                System.out.println(aplicant.toString());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
